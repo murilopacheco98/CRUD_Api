e5fa1cb8757398347ca7f11bcd21a2f2c8822030
Murilo Pacheco <murilo.pacheco99@gmail.com>
2022-10-12 19:55:20 -0300

    teste

--- src/main/java/com/crud/resource/RecadoController.java
+++ src/main/java/com/crud/resource/RecadoController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recados")
-@CrossOrigin()
+@RequestMapping()
+@CrossOrigin("*")
 public class RecadoController {
 
     @Autowired
